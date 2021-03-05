27ce23a5f45bd86013c67406fbd57442d6237d16
Herbert <herbertpansini@hotmail.com>
2021-03-05 07:13:44 -0300

    WHILE, DO-WHILE & LABELS

--- src/main/kotlin/KotlinTutorial.kt
+++ src/main/kotlin/KotlinTutorial.kt
@@ -1,16 +1,17 @@
 fun main() {
-    println("2 > 1 is ${2 > 1}")
-    println("2 < 1 is ${2 < 1}")
-    println("1 >= 1 is ${1 >= 1}")
-    println("1 == 1 is ${1 == 1}")
-    println("1 == 2 is ${1 == 2}")
-    println("1 != 2 is ${1 != 2}")
+    var i = 0
 
-    println("!(2 > 1) is ${!(2 > 1)}")
-
-    println("true && false = ${true && false}")
-    println("true || false = ${true || false}")
-
-    val condition = ((2 > 1) || (3 > 2)) && (2 > 3)
-    println(condition)
+    do {
+        println(i)
+        i++
+        continue
+        var j = 0
+        while (j < 5) {
+            println("----$j")
+            j++
+            break
+            println("I'm not printed")
+        }
+        println("I'm printed")
+    } while (i < 5)
 }
